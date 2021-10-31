8d983e4271454608b81f303dbcf44ed776b09bc6
Matteo Rigon <matteo.rigon7@gmail.com>
2021-10-31 21:51:59 +0100

    chore: remove dot

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@ export let posts
 
 <div class="container mx-auto flex justify-between px-4 flex-wrap lg:flex-nowrap">
   <div class="h-full flex flex-col justify-center w-full lg:w-2/3 px-0 md:px-4 lg:px-8 lg:pr-24 xl:pr-0 mt-12 lg:mt-0">
-    <h1 class="text-5xl md:text-6xl font-bold">Hi, I'm <span class="inline-block text-primary font-bold">Matteo</span>.</h1>
+    <h1 class="text-5xl md:text-6xl font-bold">Hi, I'm <span class="inline-block text-primary font-bold">Matteo</span></h1>
     <div class="text-xl text-gray-300">
       <p>I am a senior full stack engineer based in Italy 🇮🇹</p>
       <p>I'm an avid learner, I love technology, sports and animals.</p>
