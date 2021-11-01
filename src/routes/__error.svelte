eadcaf8a7e93dae115ee26875d70c2e23f151240
Matteo Rigon <matteo.rigon7@gmail.com>
2021-11-01 10:51:11 +0100

    feat: add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				status,
+			}
+		};
+	}
+</script>
+
+<script>
+	export let status;
+</script>
+
+<div class="container mx-auto flex flex-col  justify-center items-center">
+  <h1 class="text-8xl font-bold">{status}</h1>
+  <p class="text-4xl">oops!</p>
+  <div class="mt-12">
+    <a href="/" class="inline-block px-16 py-2 font-bold text-white uppercase">
+      <span class="inline-block mr-2 text-primary font-bold">+</span>Back home
+    </a>
+  </div>
+</div>
